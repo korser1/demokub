45c1ef10d8080bae7fccbccbe3c52b331c2f875a
Sergei Korolev <korser1@gmail.com>
2020-03-13 15:43:08 -0400

    webapi seeding, serilogging

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace WebApi
 {
@@ -13,9 +18,54 @@ namespace WebApi
         /// Entry point.
         /// </summary>
         /// <param name="args"></param>
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .Enrich.FromLogContext()
+                // uncomment to write to Azure diagnostics stream
+                //.WriteTo.File(
+                //    @"D:\home\LogFiles\Application\webapiserver.txt",
+                //    fileSizeLimitBytes: 1_000_000,
+                //    rollOnFileSizeLimit: true,
+                //    shared: true,
+                //    flushToDiskInterval: TimeSpan.FromSeconds(1))
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
+                .CreateLogger();
+
+            try
+            {
+                var seed = args.Contains("/seed");
+                if (seed)
+                {
+                    args = args.Except(new[] { "/seed" }).ToArray();
+                }
+
+                var host = CreateHostBuilder(args).Build();
+
+                if (seed)
+                {
+                    Log.Information("Seeding database...");
+                    var config = host.Services.GetRequiredService<IConfiguration>();
+                    var connectionString = config.GetConnectionString("DefaultConnection");
+                    SeedData.EnsureSeedData(connectionString);
+                    Log.Information("Done seeding database.");
+                    return 0;
+                }
+
+                Log.Information("Starting host...");
+                host.Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly.");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         /// <summary>
@@ -28,6 +78,7 @@ namespace WebApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
                     webBuilder.ConfigureAppConfiguration(c => 
                         c.AddEnvironmentVariables().AddCommandLine(args)
                     );
---
+++ WebApi/SeedData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+using Serilog;
+
+namespace WebApi
+{
+    public class SeedData
+    {
+        public static void EnsureSeedData(string connectionString)
+        {
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddDbContext<DemoDbContext>(options => options.UseSqlite(connectionString));
+
+            using var serviceProvider = services.BuildServiceProvider();
+            using var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            var context = scope.ServiceProvider.GetService<DemoDbContext>();
+            context.Database.EnsureCreated();
+            context.Database.Migrate();
+
+            if (!context.WeatherForecasts.Any())
+            {
+                var rnd = new Random();
+                var startDate = DateTime.Now.Date;
+                var forecasts = Enumerable.Range(1, 5).Select(i => new WeatherForecast
+                {
+                    Date = startDate.AddDays(i),
+                    TemperatureC = rnd.Next(-20, 55),
+                    Summary = Summaries[rnd.Next(Summaries.Length)]
+                });
+                        
+                context.WeatherForecasts.AddRange(forecasts);
+                context.SaveChanges();
+
+                Log.Debug("created records: " + context.WeatherForecasts.Count());
+            }
+            else
+            {
+                Log.Debug("data already exists!");
+            }
+        }
+        
+        private static readonly string[] Summaries = {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+    }
+}
